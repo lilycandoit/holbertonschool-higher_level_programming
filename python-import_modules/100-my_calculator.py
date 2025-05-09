6dbf0f5f1b62b2af66e3a38006131b60a0a9efc1
lily <hueduong288@gmail.com>
2025-05-09 12:17:47 +1000

    add exit if invalid operator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -14,6 +14,7 @@ def my_calculator(a, op, b):
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
     print("{} {} {} = {}".format(a, op, b, result))
 
