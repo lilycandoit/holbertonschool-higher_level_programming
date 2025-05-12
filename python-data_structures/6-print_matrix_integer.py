c85f1edfe8ad658298e69ce1cf8e883e0539f21f
lily <hueduong288@gmail.com>
2025-05-12 16:50:47 +1000

    remove space with the last character in the row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in range(len(row)):
-            end_char = ' ' if num < len(row) - 1 else ''
-            print("{:d}".format(num), end=end_char)
+        for i in range(len(row)):
+            end_char = ' ' if i  < len(row) - 1 else ''
+            print("{:d}".format(row[i]), end=end_char)
         print()
