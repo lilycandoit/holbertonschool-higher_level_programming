b93af08129d891aca91aa7ad770eb880191d9985
lily <hueduong288@gmail.com>
2025-05-12 16:52:40 +1000

    format error -remove spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            end_char = ' ' if i  < len(row) - 1 else ''
+            end_char = ' ' if i < len(row) - 1 else ''
             print("{:d}".format(row[i]), end=end_char)
         print()
