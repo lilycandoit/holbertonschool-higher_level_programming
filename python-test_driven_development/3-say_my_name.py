00f4fe0a0e6474eb26743b2267d4f185b614afa1
lily <hueduong288@gmail.com>
2025-05-16 09:25:14 +1000

    fix weird error with first_name only

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-""" This module provides a function that prints My name is <first name> <last name> """
+"""
+This module provides a function that prints My name is <first name> <last name>
+"""
+
 
 def say_my_name(first_name, last_name=""):
     """
@@ -18,5 +21,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {} {}".format(first_name, last_name))
+    if not last_name:
+        print("My name is {}".format(first_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
