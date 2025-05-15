4f26f29dd494d59fa44011c89d37e46af8451f44
lily <hueduong288@gmail.com>
2025-05-16 09:27:43 +1000

    fix Bob error

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,6 +22,6 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     if not last_name:
-        print("My name is {}".format(first_name))
+        print("My name is {} ".format(first_name))
     else:
         print("My name is {} {}".format(first_name, last_name))
