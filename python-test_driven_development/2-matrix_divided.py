c61ead0259713591b6f490ab9593f5b0e1a75e29
lily <hueduong288@gmail.com>
2025-05-15 18:19:25 +1000

    add edge test cases

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -35,7 +35,7 @@ def matrix_divided(matrix, div):
                     "matrix must be a matrix (list of lists) of integers/floats"
                     )
 
-    if not isinstance(div, (int, float)):
+    if not isinstance(div, (int, float)) or div != div:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
