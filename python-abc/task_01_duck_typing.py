f2a1d87b693bd9d8d0f436184df60da8f82ea369
lily <hueduong288@gmail.com>
2025-05-27 13:38:11 +1000

    fixed negative radius error

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -18,10 +18,10 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius**2
+        return math.pi * abs(self.radius)**2
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * abs(self.radius)
 
 
 class Rectangle(Shape):
@@ -30,10 +30,10 @@ class Rectangle(Shape):
         self.height = height
 
     def area(self):
-        return self.width * self.height
+        return abs(self.width) * abs(self.height)
 
     def perimeter(self):
-        return (self.width + self.height) * 2
+        return (abs(self.width) + abs(self.height)) * 2
 
 
 def shape_info(arg):
