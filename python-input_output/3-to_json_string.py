ac14172df8175e6c454fd14ed8c5bbae2805c824
lily <hueduong288@gmail.com>
2025-06-04 17:33:19 +1000

    fix checker error with documentation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-import json
 """
 This module defines a function that
 returns the JSON representation of an object (string)
 """
+import json
 
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of an object (string)
+    Returns the JSON representation of an object (string).
+
+    This function uses the built-in `json` module to convert a Python object
+    into a JSON-formatted string.
 
     Args:
-        my_obj: the python object
+        my_obj (any): The Python object to convert
+        (e.g., dict, list, int, str, etc.)
+
+    Returns:
+        str: A JSON-formatted string representing the input object.
     """
     return json.dumps(my_obj)
