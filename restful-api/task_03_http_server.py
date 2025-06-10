9be4862e8e74eed5e0f3a68ac9aa5b75261aeaf9
lily <hueduong288@gmail.com>
2025-06-10 21:39:13 +1000

    develop a simple API using Python with http.server module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,30 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_error(404, "Endpoint Not Found")
+
+
+# Start the server
+server = HTTPServer(("localhost", 8000), SimpleHandler)
+print("Server running at http://localhost:8000")
+server.serve_forever()
