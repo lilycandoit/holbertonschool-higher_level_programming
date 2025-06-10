4dac261a330a268f2fb7576ec44434e68533c4f2
lily <hueduong288@gmail.com>
2025-06-10 19:23:21 +1000

    task2: Consuming and processing data from an API using Python

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import csv
+
+BASE_URL = "https://jsonplaceholder.typicode.com"
+response = requests.get(f"{BASE_URL}/posts")
+
+
+def fetch_and_print_posts():
+    print("Status code: ", response.status_code)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post["title"])
+
+    else:
+        print("Failed to fetch posts")
+
+
+def fetch_and_save_posts():
+    if response.status_code == 200:
+        posts = response.json()
+
+        posts_data = [{"id": post["id"],
+                       "title": post["title"],
+                       "body": post["body"]} for post in posts]
+
+        # write to CSV using DictWriter
+        with open("posts.csv", "w") as csv_file:
+            # define column headers
+            fieldnames = ["id", "title", "body", 'userId']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            # write header row
+            writer.writeheader()
+
+            # write all post
+            writer.writerows(posts_data)
+
+        print(f"{len(posts_data)} posts successfully saved to posts.csv file")
+    else:
+        print("Failed to fetch posts:", response.status_code)
