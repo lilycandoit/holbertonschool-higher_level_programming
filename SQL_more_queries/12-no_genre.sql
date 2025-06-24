9675e455f3bd3360cd648d913607f03287758bd4
lily <hueduong288@gmail.com>
2025-06-25 08:15:53 +1000

    no genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- no genre
+
+SELECT tv_s.title, tv_g.genre_id
+FROM tv_shows AS tv_s
+LEFT JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id
+WHERE tv_g.genre_id IS NULL
+ORDER BY tv_s.title, tv_g.genre_id;
