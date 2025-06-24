4ea24828b7f01ebdfbecb7729f142d9ec23dfd6d
lily <hueduong288@gmail.com>
2025-06-24 18:13:27 +1000

    create and insert data into the table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,5 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
-DELETE FROM second_table;
-
-INSERT INTO second_table (id, name, score)
+INSERT IGNORE INTO second_table (id, name, score)
 VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
