33f0f1f604931f4110cb9a40fba1b4bf7a84a35b
lily <hueduong288@gmail.com>
2025-06-24 22:46:29 +1000

    subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,6 +5,6 @@ USE hbtn_0d_usa;
 SELECT id, name
 FROM cities
 WHERE state_id = (
-    SELECT id FROM cities WHERE name = 'California'
+    SELECT id FROM states WHERE name = 'California'
 )
 ORDER BY id;
