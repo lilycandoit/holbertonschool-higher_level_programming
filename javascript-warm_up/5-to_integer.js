13e711a71e5d3e1da0fc2b551ec3ed485dcb4e07
lily <hueduong288@gmail.com>
2025-07-17 22:08:22 +1000

    check if arg can be converted to a number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@ const arg = process.argv[2]; //first arg
 if (isNaN(Number(arg))) {
   console.log('Not a number');
 } else {
-    console.log('My number is: ' + Number(arg));
+    console.log('My number: ' + Number(arg));
 }
