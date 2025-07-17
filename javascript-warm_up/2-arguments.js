fec9a40c6617cea3de9662eb152900d49787287d
lily <hueduong288@gmail.com>
2025-07-17 20:24:42 +1000

    print command arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+console.log(process.argv);
+const args = process.argv.length;
+
+if (args === 2) {
+  console.log('No argument');
+} else if (args === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
