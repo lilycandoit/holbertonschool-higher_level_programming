7aeea4d51ff99ebf2b60a78a59bea98e77dbfb3d
lily <hueduong288@gmail.com>
2025-07-18 08:39:44 +1000

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Write a script that computes and prints a factorial
+
+const arg = Number(process.argv[2]);
+
+function factorial(n) {
+  if (n <= 1 || isNaN(n)) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(arg));
