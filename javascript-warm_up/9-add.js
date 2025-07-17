86cdcb0555b2d2e903298ebc15886fe8d6b3b174
lily <hueduong288@gmail.com>
2025-07-18 08:24:18 +1000

    sum of 2 command args

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+
+if (arg1 === undefined || arg2 === undefined || isNaN(arg1) || isNaN(arg2)) {
+  console.log('NaN');
+} else {
+  console.log(add(arg1, arg2));
+}
+
+function add(a, b) {
+  return a + b;
+}
