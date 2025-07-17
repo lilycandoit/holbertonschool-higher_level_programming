483190b766949ef3317fb37400a2a0175e32152c
lily <hueduong288@gmail.com>
2025-07-17 21:51:25 +1000

    concat 2 args

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -5,7 +5,7 @@ const arg2 = process.argv[3];
 
 if (arg1 === undefined) {
   console.log('undefined is undefined');
-} else if (arg2 === undefined){
+} else if (arg2 === undefined) {
   console.log(arg1 + ' is undefined');
 } else {
   console.log(arg1 + ' is ' + arg2);
