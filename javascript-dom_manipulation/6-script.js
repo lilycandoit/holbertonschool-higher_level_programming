b0f519ae25de137f3d51e7e667a1f7c3b5cf6ebf
lily <hueduong288@gmail.com>
2025-07-18 18:23:46 +1000

    use textContent fetchAPI

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+const characterEl = document.querySelector('#character');
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        characterEl.textContent = data.name;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
